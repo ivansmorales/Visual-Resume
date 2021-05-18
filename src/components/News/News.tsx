2c66a3741efee9dcaff08d00790828c6d74dd07d
ivanmorales <ivan.morales@atomic32.com>
2021-05-18 14:38:20 -0500

    Requests to Github API

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -1,9 +1,54 @@
 import './styles.scss';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const News = () => {
+  const [repos, setRepos] = useState(null);
+  const [lastComitHash, setLastCommitHash] = useState(null);
+
+  const getRepositories = async () => {
+    const apiUrl =
+      'https://api.github.com/users/ivansmorales/repos?sort=created';
+    axios(apiUrl).then((response) => {
+      console.log(response.data);
+      setRepos(response.data);
+    });
+  };
+
+  const getCommitActivity = async () => {
+    // First commit: 391e3a7f6f52eb71b9b7a44202b10be28033a435
+    // Last commit
+    const firstCommitHash = '391e3a7f6f52eb71b9b7a44202b10be28033a435';
+    const lastCommitUrl =
+      'https://api.github.com/repos/ivansmorales/mywebsite/git/refs/heads/master';
+    axios(lastCommitUrl).then((response) => {
+      setLastCommitHash(response.data.object.sha);
+    });
+    console.log(lastComitHash);
+    //Comparing URL
+    if (lastComitHash) {
+      const comparingURL = `https://api.github.com/repos/ivansmorales/mywebsite/compare/${firstCommitHash}...${lastComitHash}`;
+      axios(comparingURL).then((response) => {
+        console.log(response.data.total_commits + 1);
+      });
+    }
+  };
+
+  useEffect(() => {
+    getRepositories();
+    getCommitActivity();
+  }, [setRepos, setLastCommitHash]);
+
   return (
     <section className="news__section" id="news">
-      <h1>News</h1>
+      <div className="container">
+        <h1>
+          Github <span>Contributions</span>
+        </h1>
+        <div className="last-repos">
+          <h2>Últimos tres repositorios</h2>
+        </div>
+      </div>
     </section>
   );
 };
